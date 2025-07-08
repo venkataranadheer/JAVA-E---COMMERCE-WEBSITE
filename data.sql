d760ffe7c35d8f4538ff358b7ef63db1680e6a86
venkataranadheer <venkataranadheer123@gmail.com>
2025-07-08 17:47:27 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCT (name, description, price) VALUES ('Laptop', 'Gaming Laptop', 89999.99);
+INSERT INTO PRODUCT (name, description, price) VALUES ('Phone', 'Android Phone', 19999.00);
+INSERT INTO PRODUCT (name, description, price) VALUES ('Keyboard', 'Mechanical Keyboard', 2999.50);
