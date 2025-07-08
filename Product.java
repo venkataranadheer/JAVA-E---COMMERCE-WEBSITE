8d9debca43ffe8779f69d181b8f356614e73f5dd
venkataranadheer <venkataranadheer123@gmail.com>
2025-07-08 17:28:41 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,18 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+}
